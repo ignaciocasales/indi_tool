0fbbe6816653869c0032866e8e24e3879078454d
ignaciocasales <casales.ignacio@gmail.com>
2024-06-10 21:07:20 -0300

    Separate main from app

---
+++ lib/app.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:indi_tool/consts.dart';
+import 'package:indi_tool/screens/workspace/workspace.dart';
+
+class IndiApp extends StatelessWidget {
+  const IndiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = ThemeData(
+      fontFamily: GoogleFonts.openSans().fontFamily,
+      colorSchemeSeed: Colors.deepPurple,
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      visualDensity: VisualDensity.compact,
+    );
+
+    return MaterialApp(
+      title: kAppName,
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    constraints: const BoxConstraints(
+                      minHeight: 32,
+                    ),
+                    color: Theme.of(context).colorScheme.primary,
+                    child: const Center(
+                      child: Text(kShortAppName,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Expanded(
+              child: Row(
+                children: [
+                  Workspace(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:indi_tool/consts.dart';
-import 'package:indi_tool/screens/workspace/workspace.dart';
+import 'package:indi_tool/app.dart';
+import 'package:indi_tool/window.dart';
 
 void main() {
-  runApp(const Indi());
-}
+  setupWindow();
 
-class Indi extends StatelessWidget {
-  const Indi({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: kAppName,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: GoogleFonts.openSans().fontFamily,
-        colorSchemeSeed: Colors.deepPurple,
-        useMaterial3: true,
-        brightness: Brightness.dark,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    constraints: const BoxConstraints(
-                      minHeight: 32,
-                    ),
-                    color: Theme.of(context).colorScheme.primary,
-                    child: const Center(
-                      child: Text(
-                        kShortAppName,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        )
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const Expanded(
-              child: Row(
-                children: [
-                  Workspace(),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  runApp(
+    // Main app widget
+    const IndiApp(),
+  );
 }
